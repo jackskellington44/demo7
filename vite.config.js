f505df10e3a9c8a45728f2dd599959ca6d7a21d9
jackskellington44 <jackskellington4@duck.com>
2026-05-06 01:38:34 +0200

    demo change

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-  base: "/demo1/",
+  base: "/demo7/",
   build: {
     rollupOptions: {
       input: {
